58eac330f4a1bf827c98a25aca93a992c9579fff
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2021-01-15 22:16:40 +0300

    PnPjs 2.1.0 true stream upload

--- src/Upload.ts
+++ src/Upload.ts
@@ -1,10 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { sp, IFileAddResult, IFileUploadProgressData } from '@pnp/sp-commonjs/presets/all';
-import NodeFetchClient from 'pnp-auth/lib/NodeFetchClient';
+
 import { IAuthContext } from 'node-sp-auth-config';
+import { sp, Web, IWeb, IFileAddResult, IFileUploadProgressData } from '@pnp/sp-commonjs/presets/all';
+import '@pnp/nodejs-commonjs'; // include extensions to support Node.js streams
+
+import NodeFetchClient from 'pnp-auth/lib/NodeFetchClient';
 
 export class Upload {
+
+  private web: IWeb;
 
   constructor(context: IAuthContext) {
     const fetchFactory = new NodeFetchClient(context.authOptions, context.siteUrl);
@@ -13,51 +18,34 @@ export class Upload {
         fetchClientFactory: () => fetchFactory
       }
     });
+    this.web = Web(context.siteUrl);
   }
 
-  public addChunked(
+  public async addChunked(
     folderRelativeUrl: string,
     filePath: string,
-    progress?: (data: IFileUploadProgressData) => void
+    progress?: (data: IFileUploadProgressData) => void,
+    chunkSize = 10485760
   ): Promise<IFileAddResult> {
-    const fileName = path.parse(filePath).name + path.parse(filePath).ext;
-    return this.readFile(filePath)
-      .then((content) => {
-        return sp.web.getFolderByServerRelativeUrl(folderRelativeUrl)
-          .files.addChunked(fileName, content as any, progress, true);
-      });
-  }
 
-  // File size (request body) is limited to 262144000 bytes
-  public addStream(folderRelativeUrl: string, filePath: string): Promise<IFileAddResult> {
-    const fileName = path.parse(filePath).name + path.parse(filePath).ext;
-    const rs = fs.createReadStream(filePath);
-    return sp.web.getFolderByServerRelativeUrl(folderRelativeUrl)
-      .files
-      .configure({
-        headers: {
-          'Content-Length': `${fs.statSync(filePath).size}`
-        }
-      })
-      .add(fileName, rs);
-  }
+    let ticker: (data: IFileUploadProgressData) => void = 'function' === typeof progress ? (() => {
+      const stats = fs.statSync(filePath);
+      // In a stream object there is no `size` property, so IFileUploadProgressData object can't know
+      // `fileSize` and `totalBlocks` without externally provided size received e.g. with fs.stat.
+      // This wraps provided `progress` callback and enriches data argument to contain missed props.
+      return (data: IFileUploadProgressData): void => {
+        data.fileSize = stats.size;
+        data.totalBlocks = data.totalBlocks ??
+          parseInt((data.fileSize / chunkSize).toString(), 10) + ((data.fileSize % chunkSize === 0) ? 1 : 0);
+        progress(data);
+      };
+    })() : null;
 
-  private readFile(filePath: string): Promise<Buffer> {
-    return new Promise((resolve, reject) => {
-      fs.stat(filePath, (statErr, stats) => {
-        if (statErr) {
-          return reject(statErr);
-        }
-        fs.readFile(filePath, (readErr, buffer) => {
-          if (readErr) {
-            return reject(readErr);
-          }
-          // This is required to tream Buffer the same way as Blob
-          (buffer as any).size = stats.size;
-          resolve(buffer);
-        });
-      });
-    });
+    const fileName = path.parse(filePath).name + path.parse(filePath).ext;
+    const rs = fs.createReadStream(filePath, { highWaterMark: chunkSize }); // highWaterMark must be equal to chunkSize
+
+    return this.web.getFolderByServerRelativeUrl(folderRelativeUrl)
+      .files.addChunked(fileName, rs, ticker, true, chunkSize)
   }
 
 }
